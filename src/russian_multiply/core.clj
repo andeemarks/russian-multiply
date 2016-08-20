2fd1a688f8cc8ba301a67ad04e46b86ea47fe809
Andy Marks <amarks@thoughtworks.com>
2016-08-20 20:47:27 +1000

    Repeated integer division seems OK

--- src/russian_multiply/core.clj
+++ src/russian_multiply/core.clj
@@ -1,6 +1,13 @@
 (ns russian-multiply.core
   (:gen-class))
 
+(defn divide [divisor]
+    (loop [factor divisor
+            even-factors '[]]
+      (if (< factor 1)
+        even-factors
+       (recur (quot factor 2) (conj even-factors factor)))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
--- test/russian_multiply/core_test.clj
+++ test/russian_multiply/core_test.clj
@@ -3,5 +3,7 @@
             [russian-multiply.core :refer :all]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "divide"
+    (is (= [123 61 30 15 7 3 1] (divide 123)))
+    (is (= [2 1] (divide 2)))
+    (is (= [1] (divide 1)))))
