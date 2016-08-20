13caa368dece4d7f8ce997cdfabd849154a44a9d
Andy Marks <amarks@thoughtworks.com>
2016-08-20 19:55:15 +1000

    After lein new app

---
+++ test/russian_multiply/core_test.clj
@@ -0,0 +1,7 @@
+(ns russian-multiply.core-test
+  (:require [clojure.test :refer :all]
+            [russian-multiply.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
